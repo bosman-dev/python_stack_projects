d83cb74d36b688965687c41001c539a9c57410f5
Osman Bangura <osmanbangura288@gmail.com>
2022-09-26 09:49:17 +0100

    edit schools.view file

--- crud_projects/schools_app/views.py
+++ crud_projects/schools_app/views.py
@@ -4,30 +4,30 @@ from django.views.generic import (
                                 ListView,DetailView,
                                 CreateView,DeleteView,
                                 UpdateView)
-from . import models
+from .models import *
 
 class SchoolListView(ListView):
-    model = models.School
+    model = School
     # Default would be 'school_list'
     # Example of making your own:
     # context_object_name = 'schools'
     
 class SchoolDetailView(DetailView):
     context_object_name = 'school_details'
-    model = models.School
+    model = School
     template_name = 'basic_app/school_detail.html'
 
 
 class SchoolCreateView(CreateView):
     fields = ("name","principal","location")
-    model = models.School
+    model = School
 
 
 class SchoolUpdateView(UpdateView):
     fields = ("name","principal")
-    model = models.School
+    model = School
 
 class SchoolDeleteView(DeleteView):
-    model = models.School
+    model = School
     success_url = reverse_lazy("basic_app:list")
 
